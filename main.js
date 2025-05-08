f714ecbe3f5264cbeeac74e7d8986c534e31e68c
huyilin <v_huyilin@baidu.com>
2025-05-08 17:07:06 +0800

    feat

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, dialog, ipcMain } = require("electron");
+const path = require("path");
 
-const createWindow = () => {
-  const win = new BrowserWindow({
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true, // 开启 Node.js 集成
+      webSecurity: false, // 禁用 Web 安全策略，允许加载本地文件
+    },
+  });
+  win.webContents.openDevTools();
 
-  // win.loadFile('index.html')
-  win.loadURL('http://localhost:5173')
+  win.loadURL("http://localhost:5173"); // 假设是 Vue 项目的本地服务器地址
 }
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+
+  ipcMain.handle("dialog:openFile", async () => {
+    const result = await dialog.showOpenDialog(win, {
+      properties: ["openFile"],
+    });
+
+    return result.filePaths[0]; // 返回选择的文件路径
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
--- ui/src/views/Publish.vue
+++ ui/src/views/Publish.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="publish-container">
     <a-form :model="form" :style="{ width: '70%' }" @submit="handleSubmit">
-      <a-form-item field="name" label="选择视频">
-        <a-upload draggable action="/" />
+      <a-form-item field="url" label="选择视频">
+        <div class="video-upload" @click="openFileDialog" v-if="!form.url">点击选择视频</div>
+        <video class="video-upload" :src="`file://${form.url}`" controls v-else></video>
       </a-form-item>
 
       <a-form-item field="name" label="视频描述">
@@ -22,13 +23,18 @@
 import { reactive } from 'vue'
 
 const form = reactive({
+  url: '',
   desc: '',
-  post: '',
-  isRead: false,
 })
 
 const handleSubmit = (data) => {
   console.log(data)
+}
+
+const openFileDialog = async () => {
+  const filePath = await window.electron.invoke('dialog:openFile')
+  console.log(filePath);
+  form.url = filePath
 }
 </script>
 
@@ -38,5 +44,16 @@ const handleSubmit = (data) => {
   justify-content: center;
   align-items: center;
   height: 100%;
+
+  .video-upload {
+    width: 100%;
+    height: 150px;
+    border-radius: 4px;
+    cursor: pointer;
+    display: flex;
+    background-color: rgb(242, 243, 245);
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
