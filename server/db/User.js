f1a803455f98dd792ae62cbc32058bff94487181
hyl <3307378337@qq.com>
2025-07-28 08:20:58 +0800

    feat

---
+++ server/db/User.js
@@ -0,0 +1,16 @@
+const { EntitySchema } = require("typeorm");
+
+module.exports = new EntitySchema({
+  name: "User",
+  tableName: "users",
+  columns: {
+    id: {
+      primary: true,
+      type: "int",
+      generated: true,
+    },
+    name: {
+      type: "varchar",
+    },
+  },
+});
